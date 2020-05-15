174529d033ab9c6c6948bf830e248af43b3c22b2
Lasse Kelling <lasse@kelling.de>
2020-05-15 20:49:11 +0200

    Beta-Touchscreen-Support hinzugefügt

---
+++ Assets/Scripts/CameraAspectKeeper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAspectKeeper : MonoBehaviour {
+  public float orthographicSize = 5;
+  public float aspect = 1.33333f;
+  void Start() {
+    Camera.main.projectionMatrix = Matrix4x4.Ortho(-orthographicSize * aspect,
+      orthographicSize * aspect, -orthographicSize,
+      orthographicSize, camera.nearClipPlane, camera.farClipPlane);
+  }
+}
