3d8abdecb0c09af2ec00314035b84759318f6fff
Lasse Kelling <lasse@kelling.de>
2020-05-26 23:01:30 +0200

    Unity Analytics und Unity Ads eingebunden

---
+++ Assets/Scripts/InitializeAdsScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class InitializeAdsScript : MonoBehaviour {
+  [SerializeField]
+  public bool testMode = false;
+
+  string gameIdApple = "3622512";
+  string gameIdGoogle = "3622513";
+
+  void Start() {
+    if(Application.platform == RuntimePlatform.IPhonePlayer)
+      Advertisement.Initialize(gameIdApple, testMode);
+      if(Application.platform == RuntimePlatform.Android)
+        Advertisement.Initialize(gameIdGoogle, testMode);
+  }
+}
