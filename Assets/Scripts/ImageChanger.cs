5827321da82ffb03171bb0350612c5fb33579e1c
Lasse Kelling <lasse@kelling.de>
2020-06-22 02:50:22 +0200

    Neuer ImageChanger

--- Assets/Scripts/ImageChanger.cs
+++ Assets/Scripts/ImageChanger.cs
@@ -4,36 +4,36 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ImageChanger : MonoBehaviour {
-
+  public Image[] images;
   [Range(0.1f,2f)]
   public float speed = 0.5f;
   public bool active = true;
-  public Sprite[] images;
-  public Sprite clearImage;
 
-  private Image parent;
   private float deltaTime;
   private int i = 0;
   private bool updateFlag = false;
+  private Color white = Color.white;
+  private Color trans = new Color(0,0,0,0);
   void Start() {
-    parent = GetComponent<Image>();
+    foreach(Image im in images) {
+      im.color = trans;
+    }
   }
   void Update() {
     deltaTime += Time.deltaTime;
     if(updateFlag) {
-      parent.overrideSprite = nextImage();
+      if(i >= images.Length)
+        i = 0;
+      foreach(Image im in images) {
+        im.color = trans;
+      }
+      images[i].color = white;
+      i++;
       updateFlag = false;
     }
     if(deltaTime > speed) {
       deltaTime -= speed;
       updateFlag = true;
-      parent.overrideSprite = clearImage;
     }
-    parent.enabled = active;
-  }
-  Sprite nextImage() {
-    if(i >= images.Length)
-      i = 0;
-    return images[i++];
   }
 }
